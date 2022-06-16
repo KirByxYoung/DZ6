63c3a134ec2ce372d1406dca201c392bb1f6647a
KirByxYoung <istegomen@ya.ru>
2022-06-16 16:00:50 +0400

    commit

---
+++ DZ6/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _emeny;
+    [SerializeField] private Vector3[] _positions;
+
+    private int _quantityPositions;
+
+    private void Start()
+    {
+        _quantityPositions = _positions.Length;
+        StartCoroutine(Spawn());
+    }
+
+    public IEnumerator Spawn()
+    {
+        int index = Random.Range(0, _quantityPositions);
+
+        GameObject newEnemy = Instantiate(_emeny, _positions[index], Quaternion.identity);
+
+        yield return new WaitForSeconds(2);
+
+        Destroy(newEnemy);
+
+        StartCoroutine(Spawn());
+    }
+}
